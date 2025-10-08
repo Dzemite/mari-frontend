4f2da988d766d63eab59a94f301aaf14775f6b19
Roman Nikolaev <rnikolaev@teh-lab.ru>
2025-10-08 14:08:13 +0300

    add ico and next block

---
+++ src/app/pages/main-page/education/education.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education',
+  imports: [],
+  templateUrl: './education.component.html',
+  styleUrl: './education.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EducationComponent { }
